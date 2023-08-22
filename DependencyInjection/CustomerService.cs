535306e3b9ee6e0e1f08355ac11f329f76e7ed57
Abdul Kuzhiyil <Abdul.Kuzhiyil@ii-vi.com>
2023-08-22 16:45:12 +0100

    DI Code implementation for Customer service

---
+++ DependencyInjection/CustomerService.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DependencyInjection
+{
+    public class CustomerService : ICustomerService
+
+    {
+        private List<Customer> customers;
+     
+        public Customer GetCustomerById(int id)
+        {
+
+            return customers.FirstOrDefault(c => c.Id == id);
+        }
+        public void PopulateCustomers()
+        {
+            customers = new List<Customer>
+            {
+                new Customer { Id=1, Name = "Abdul Salam"},
+                new Customer { Id=2, Name = "Dileep D"},
+                new Customer { Id=3, Name = "Jees P"},
+                new Customer { Id=4, Name = "Anoop Aug"},
+                new Customer { Id=5, Name = "Arun"},
+            };
+        }
+    }
+}
--- DependencyInjection/Program.cs
+++ DependencyInjection/Program.cs
@@ -1,7 +1,27 @@
-﻿internal class Program
+﻿using DependencyInjection;
+using Microsoft.Extensions.DependencyInjection;
+
+internal class Program
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        var serviceCollection = new ServiceCollection();
+        ConfigurationServices(serviceCollection);
+
+        var serviceProvider = serviceCollection.BuildServiceProvider();
+
+        var myCustomerService = serviceProvider.GetService<ICustomerService>();
+        //populate customers first
+        myCustomerService.PopulateCustomers();
+        var customer = myCustomerService.GetCustomerById(1);
+
+        Console.WriteLine($"Customer Name with Id {customer.Id} is {customer.Name}");
+
+    }
+
+
+    private static void ConfigurationServices(IServiceCollection services)
+    {
+        services.AddTransient<ICustomerService,CustomerService>();
     }
 }
